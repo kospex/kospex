c951e63ed1a8f8191a98500f86efb3222edd2300
sabbaticas <109841928+sabbaticas@users.noreply.github.com>
2023-12-04 14:24:26 +1100

    clean up debug code

--- kospex_query.py
+++ kospex_query.py
@@ -485,7 +485,6 @@ class KospexQuery:
             row['last_seen'] = KospexUtils.days_ago(row['last_commit'])
             row['first_seen'] = KospexUtils.days_ago(row['first_commit'])
             row['days_active'] = int(row.get('first_seen',0)) - int(row.get('last_seen',0))
-            print(row['days_active'])
             results.append(row)
 
         return results
