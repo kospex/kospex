8e065d9be3ad6e205b75ec3a567831b1c88364d2
sabbaticas <109841928+sabbaticas@users.noreply.github.com>
2025-07-21 21:51:03 +1000

    Version change for release 0.0.20

--- src/kospex_core.py
+++ src/kospex_core.py
@@ -33,7 +33,7 @@ class Kospex:
     """
     kospex core functionality
     """
-    VERSION = "0.0.19" # This value should align with the pyproject.toml version for pip
+    VERSION = "0.0.20" # This value should align with the pyproject.toml version for pip
     def __init__(self):
 
         self.original_cwd = None
