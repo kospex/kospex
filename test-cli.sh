e7f5afd0af975888b6e5045389809d8a7025f02b
sabbaticas <109841928+sabbaticas@users.noreply.github.com>
2025-08-14 17:32:51 +1000

    Fixed a CLI bug
    -create needed a -- instead, caused the build to fail

--- test-cli.sh
+++ test-cli.sh
@@ -3,11 +3,10 @@
 # This script is used to test the CLI interface of kospex
 set -e
 
-kospex init -create
+kospex init --create
 
 # check that kospex runs without arguments
 kospex
 
 # check that kospex summary runs without arguments
 kospex summary
-
